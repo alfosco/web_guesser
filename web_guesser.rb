be760811be18b85fede96667a32e7fee39c48464
Alex Fosco <alex.hf88@gmail.com>
2017-01-19 17:11:52 -0700

    I3: number guessing game complete; still have extensions to complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,24 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+SECRET_NUMBER = rand(100)
+
+def check_guess(guess)
+  if guess.to_i - SECRET_NUMBER > 5
+    "Way too high!"
+  elsif SECRET_NUMBER - guess.to_i > 5
+    "Way too low!"
+  elsif guess.to_i > SECRET_NUMBER
+    "Too high!"
+  elsif guess.to_i < SECRET_NUMBER
+    "Too low!"
+  else
+    "You got it right!<br />The SECRET NUMBER is #{SECRET_NUMBER}"
+  end
+end
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"]
+  message = check_guess(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
 end
